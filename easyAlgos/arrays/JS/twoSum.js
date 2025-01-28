b8b564fe1cf3e0b696e0b766e1c05b9690a219d8
oscar sanchez <00oscarsanchez00@gmail.com>
2025-01-27 20:04:50 -0600

    Getting these algos to run at 100% or as close to 100% run time on leetcode.

--- easyAlgos/arrays/JS/twoSum.js
+++ easyAlgos/arrays/JS/twoSum.js
@@ -1,78 +1,84 @@
-const ints1 = [1, 3, 4, 5, 7, 9];
-const ints2 = [9, 5, 7, 4, 1, 3]; // EDGE CASE: WHAT IF THE ARRAY OF INTEGERS WAS NOT SORTED
 /*
-  TASK: WRITE A FUNCTION THAT WILL TAKE AN ARRAY OF SORTED INTEGERS AND A TARGET NUMBER.
-        FIND ANY PAIR OF ELEMENTS THAT ADD UP TO THE TARGET
+1. Two Sum
 
-  APPROACH: ATTEMPT TO SOLVE THE PROBLEM WITH A TIME COMPLEXITY OF O(N), SPACE COMPLEXITY O(N)
-            1. CREATE A HASH TABLE TO STORE KEY VALUE PAIRS AND A PLACEHOLDER VARIABLE
-            2. LOOP OVER THE ARRAY ONLY ONCE
-            4. ON EACH LOOP SUBTRACT THE INTEGER/ELEMENT AT THE CURRENT INDEX FROM THE TARGET CALL ITS VALUE  difference
-            3. ASSIGN DIFFERENCE TO BE THE KEY IN THE HASH TABLE FOR THE VALUE OF THE ELEMENT/INTEGER AT THE CURRENT INDEX
-            4. NEXT TIME YOU LOOP, IF THE HASH TABLE HAS THE KEY OF THE CURRENT ELEMENT, YOU'VE FOUND YOUR PAIR
+Given an array of integers nums and an integer target, return indices of the two
+numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not
+use the same element twice.
+You can return the answer in any order.
+
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
 */
 
+// time complexity: O(n)
+// this version of using a regular object as a hash map is slower than using a new Map object
 /*
-nums array = const ints1 = [1, 3, 4, 5, 7, 9];
-target = 10                 9, 7, 6, 5, 3, 1
-results = [ [1, 9], [7, 3] ]
-
-1.  create an object to keep a mapping of index to number and an array to enter results into
-2.  loop over the nums array
-3.  find the difference between the targetNum and the current num
-4.  use the difference as a key in hash to the current num. if that key is found later, it means that you found your pair
-5.  obviously, the sought pairs will not be found until at least the second iteration, so check for solutions when loop starts up again
-
-while looping
-get the difference
-first iteration: 10 - 1  = 9
-hash[9] = 1  // let me know if you see a 9 while looping
-second iteration: 10 - 3  = 7
-hash[7] = 3  // let me know if you see a 7 while looping
-
-    {
-      9: 1, 7: 3, 6: 4, 5: 5, 3: 7, 1: 9
-    }
+According to google search with AI assisting:
+"In JavaScript, a regular object and a "new Map" object both store key-value pairs,
+but the key difference is that a Map is generally faster for operations like adding,
+removing, and accessing data, especially when dealing with frequent changes,
+because it's specifically designed for efficient key-value lookups and maintains
+insertion order, while a regular object is not optimized for these actions and has
+limitations on key types (only allowing strings and symbols)."
 */
-
-const twoSum = (numsArr, targetNum) => {
-  const hash = {},
-    resp = [];
-  for (let i = 0; i < numsArr.length; ++i) {
-    if (hash[numsArr[i]]) {
-      resp[resp.length] = [hash[numsArr[i]], numsArr[i]];
-    }
-    let difference = targetNum - numsArr[i];
-    hash[difference] = numsArr[i];
+const twoSum = (nums, target) => {
+  for (let i = 0, hash = {}; i < nums.length; ++i) {
+    if (hash[nums[i]] !== undefined) return [hash[nums[i]], i];
+    hash[target - nums[i]] = i;
   }
-  return resp;
+  return [];
 };
 
-// const twoSum = (ints, tar) => {
-//   res = [];
-//   if (ints[1]) {
-//     const hash = {};
-//     let difference;
-//     for (let i = 0; i < ints.length; ++i) {
-//       if (hash[ints[i]]) res[res.length] = [ints[i], hash[ints[i]]];
-//       difference = tar - ints[i];
-//       hash[difference] = ints[i];
-//     }
-//   }
-//   return res;
-// };
+// time complexity: O(n)
+// faster version than the one above because it uses a new Map object, which has faster insertion,
+// deleting and retrieving of key/values
+const twoSumV2 = (nums, target) => {
+  for (let i = 0, hash = new Map(); i < nums.length; ++i) {
+    if (hash.has(nums[i])) return [hash.get(nums[i]), i];
+    hash.set(target - nums[i], i);
+  }
+  return [];
+};
 
-console.log(
-  'there should be more than one pair that add up to target: ',
-  twoSum(ints1, 10)
-);
-console.log(
-  'there should be more than one pair that add up to target: ',
-  twoSum(ints2, 10)
-);
+// time complexity: O(n)
+// this version of using an array as a hash map is slower than using a new Map object
+// but faster than using a regular object
+const twoSumV3 = (nums, target) => {
+  for (let i = 0, hash = []; i < nums.length; ++i) {
+    if (hash[nums[i]] !== undefined) return [hash[nums[i]], i];
+    hash[target - nums[i]] = i;
+  }
+  return [];
+};
 
-console.log('an empty array: ', twoSum([], 10));
-console.log('an array with 1 number/element: ', twoSum([1], 10));
+const nums1 = [2, 7, 11, 15],
+  target1 = 9;
+const nums2 = [3, 2, 4],
+  target2 = 6;
+const nums3 = [3, 3],
+  target3 = 6;
 
-// TARGET = 10
-// FOR THE FIRST ELEMENT: 10 - 1 = 9, SO THE PAIR EQUAL TO TARGET WOULD BE ( 1, 9 ) IN THIS CASE
+console.log(twoSum(nums1, target1)); // [0, 1]
+console.log(twoSum(nums2, target2)); // [1, 2]
+console.log(twoSum(nums3, target3)); // [0, 1]
